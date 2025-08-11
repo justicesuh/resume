901dd205435edf4bca31ddb6682f2370dacf56fb
Justice Suh <justice.suh@gmail.com>
2025-08-11 19:19:02 -0400

    Add bottom padding

--- generate.py
+++ generate.py
@@ -31,23 +31,26 @@ class Resume:
         with open(self.config, 'r') as f:
             self.data = yaml.safe_load(f)
 
+    def set_font_size(self, size: float):
+        self.pdf.set_font_size(size)
+
     def add_heading(self, text: str, font_size: float):
         self.pdf.set_font_size(font_size)
         self.pdf.cell(self.page_width, self.line_height, text=text, align=Align.C, new_x=XPos.LMARGIN, new_y=YPos.NEXT)
 
-    def add_row(self, data: list[str]):
+    def add_row(self, data: list[str], padding_bottom: float = 0):
         row = []
         colspan = 2 if len(data) == 1 else 1
         for text, align in zip(data, [Align.L, Align.R]):
-            row.append((text, align, colspan))
+            row.append((text, align, colspan, padding_bottom))
         self.rows.append(row)
 
     def _generate_table(self):
         with self.pdf.table(col_widths=(6, 4), first_row_as_headings=False, markdown=True, line_height=6) as table:
-            for cell in self.rows:
-                row = table.row()
-                for text, align, colspan in cell:
-                    row.cell(text, colspan=colspan, border=CellBordersLayout.NONE, align=align)
+            for row in self.rows:
+                table_row = table.row()
+                for text, align, colspan, padding_bottom in row:
+                    table_row.cell(text, colspan=colspan, border=CellBordersLayout.NONE, align=align, style=None, padding=(0, 0, padding_bottom, 0))
 
     def output(self):
         self._generate_table()
@@ -67,8 +70,7 @@ def main():
             f"**{experience['company']}** {Resume.ENDASH} {experience['title']}",
             f"{experience['start_date']} {Resume.ENDASH} {experience['end_date']}"
         ])
-        resume.add_row([f"{Resume.BULLET}\t\t" + f"\n{Resume.BULLET}\t\t".join(experience['highlights'])])
-    resume.add_row('')
+        resume.add_row([f"{Resume.BULLET}\t\t" + f"\n{Resume.BULLET}\t\t".join(experience['highlights'])], 3)
 
     education = resume.data['sections']['education']
     resume.add_row(['**EDUCATION**'])
