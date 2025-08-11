161dcdda16c449b344c984b8a0ab96d74f2e4c6c
Justice Suh <justice.suh@gmail.com>
2025-08-10 23:12:58 -0400

    Add empty table

--- generate.py
+++ generate.py
@@ -16,6 +16,14 @@ def main():
     pdf.set_font_size(12)
     pdf.cell(pdf.w - pdf.l_margin - pdf.r_margin, 8, text=data['heading'], align=Align.C, new_x=XPos.LMARGIN, new_y=YPos.NEXT)
 
+    num_rows = len(data['sections']) + (len(data['sections']['experiences']) * 2) + len(data['sections']['education'])
+    print(num_rows)
+    with pdf.table(col_widths=(1, 1)) as table:
+        for _ in range(num_rows):
+            row = table.row()
+            for datum in [' ', ' ']:
+                row.cell(datum)
+
     pdf.output('resume.pdf')
 
 
